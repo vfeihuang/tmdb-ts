016c9f6e2b3845df6747fa3c0041e844bd476428
zyh <721652234@qq.com>
2026-01-20 20:12:19 +0800

    初始时，增加链接和默认语言的配置

--- src/api.ts
+++ src/api.ts
@@ -1,17 +1,29 @@
 // Using native fetch API (available in Node 18+ and all modern browsers)
 import { parseOptions } from './utils';
-import { ErrorResponse } from './types';
-import { BASE_URL_V3 } from './common/constants';
+import { ErrorResponse } from './types'; 
 
 export class Api {
-  constructor(private accessToken: string) {
-    this.accessToken = accessToken;
+	protected baseurl: string = 'https://api.themoviedb.org';
+	protected accessToken: string="";
+	protected language: string;
+  constructor(private apikey: string,private url: string="",private lang: string="en-US") {
+    this.accessToken = apikey;
+    if(url&&url!=""){
+      this.baseurl = url;
+    }
+    this.language = lang;
   }
 
   /* eslint-disable  @typescript-eslint/no-explicit-any */
   async get<T>(path: string, options?: Record<string, any>): Promise<T> {
+    if(options==null ){
+      options={language:this.language};
+    }
+    else if(!Object.keys(options).includes("language")){
+      options["language"] = this.language;
+    }
     const params = parseOptions(options);
-    const response = await fetch(`${BASE_URL_V3}${path}?${params}`, {
+    const response = await fetch(`${this.baseurl}/3${path}?${params}`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${this.accessToken}`,
